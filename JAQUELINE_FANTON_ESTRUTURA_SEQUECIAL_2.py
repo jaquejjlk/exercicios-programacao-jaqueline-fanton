564ec0673e69bd77dc1762754856409f38a28465
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2025-10-07 21:51:36 -0300

    2

--- JAQUELINE_FANTON_ESTRUTURA_SEQUECIAL_2.py
+++ JAQUELINE_FANTON_ESTRUTURA_SEQUECIAL_2.py
@@ -1,4 +1,4 @@
 #Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
 #Desenvolvido por: JAQUELINE FANTON
-número = input("Informe um número:")
+número = input("Digite um número:")
 print("O número informado",número)
