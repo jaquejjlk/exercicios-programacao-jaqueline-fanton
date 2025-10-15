f68f458e8f4a524b0d31d8994e57c7e32f98777b
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2025-10-15 20:29:52 -0300

    JQ

--- JAQUELINE_FANTON_ESTRUTURA_SEQUENCIAL_4.py
+++ JAQUELINE_FANTON_ESTRUTURA_SEQUENCIAL_4.py
@@ -1,9 +1,9 @@
 #Faça um programa que peça as 4 notas bimestrais e mostre a média.
 #Desenvolvido por: JAQUELINE FANTON
 
-numero1=float(input("Qual a 1ª nota? "))
-numero2=float(input("Qual a 2ª nota? "))
-numero3=float(input("Qual a 3ª nota? "))
-numero4=float(input("Qual a 4ª nota? "))
-media=(numero1+numero2+numero3+numero4)/4
+numero1 = float(input("Qual a 1ª nota?"))
+numero2 = float(input("Qual a 2ª nota?"))
+numero3 = float(input("Qual a 3ª nota?"))
+numero4 = float(input("Qual a 4ª nota?"))
+media = (numero1+numero2+numero3+numero4)/4
 print(media)
