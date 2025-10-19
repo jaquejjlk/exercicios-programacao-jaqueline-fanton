302f9793ba93c709e5b58a0904ae9822acece8f0
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2025-10-19 13:24:25 -0300

    Update JAQUELINE_FANTON_ESTRUTURA_SEQUENCIAL_9.py

--- JAQUELINE_FANTON_ESTRUTURA_SEQUENCIAL_9.py
+++ JAQUELINE_FANTON_ESTRUTURA_SEQUENCIAL_9.py
@@ -4,6 +4,6 @@
 #Desenvolvido por: JAQUELINE FANTON
 
 fahrenheit = float(input("Qual a temperatura em graus Fahrenheit:"))
-print(fahrenheit)
+print(fahrenheit)#commit
 celsius = 5 * ((fahrenheit-32) / 9)
 print("em graus Celsius é:",celsius)
