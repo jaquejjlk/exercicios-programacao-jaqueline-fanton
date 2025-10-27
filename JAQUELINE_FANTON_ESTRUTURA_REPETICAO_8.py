03e19c1a55fc0dfeb8127d3122bf93c259aa7b7e
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2025-10-27 20:37:20 -0300

    JQ

--- JAQUELINE_FANTON_ESTRUTURA_REPETICAO_8.py
+++ JAQUELINE_FANTON_ESTRUTURA_REPETICAO_8.py
@@ -8,5 +8,5 @@ numero4=float(input("Digite o quarto número:"))
 numero5=float(input("Digite o quinto número:"))
 soma=(numero1+numero2+numero3+numero4+numero5)
 print("A soma dos número é:",soma)
-media = (numero1+numero2+numero3+numero4+numero5)/5
+media=(numero1+numero2+numero3+numero4+numero5)/5
 print("E a Média é:",media)
